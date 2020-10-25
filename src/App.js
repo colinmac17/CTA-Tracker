dafeab966a271eb97b7d105b71825c0727b48a9e
Colin McAtee <colin.g.mcatee@gmail.com>
2020-10-25 07:29:18 -0500

    updates to layout

--- src/App.js
+++ src/App.js
@@ -10,38 +10,59 @@ import {Favorites} from './Components/Favorites';
 import Weather from './Components/Weather';
 import {NoMatch} from './Components/NoMatch';
 import {Nav} from './Components/Nav';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
+//@see https://material-ui.com/components/grid/#grid-with-breakpoints for documentation
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}))
+
+/**
+ * We use the Grid component to make a responsive layout
+ * On small screens and above the navbar is the leftmost three columns and the app components are the right most 9.
+ * On mobile devices, the navbar is on top with components below
+ */
 function App() {
+  const classes = useStyles();
   return (
     <Router>
-      <div>
-        <Nav />
-        <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-            <Route exact path="/favorites">
-              <Favorites />
-            </Route>
-            <Route exact path="/maps">
-              <Maps />
-            </Route>
-            <Route exact path="/bus-eta">
-              <BusEta />
-            </Route>
-            <Route exact path="/train-eta">
-              <TrainEta />
-            </Route>
-            <Route exact path="/weather">
-              <Weather />
-            </Route>
-            <Route path="*">
-              <NoMatch />
-            </Route>
-        </Switch>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={3}>
+            <Nav />
+          </Grid>
+          <Grid item xs={12} sm={9}>
+            <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route exact path="/about">
+                  <About />
+                </Route>
+                <Route exact path="/favorites">
+                  <Favorites />
+                </Route>
+                <Route exact path="/maps">
+                  <Maps />
+                </Route>
+                <Route exact path="/bus-eta">
+                  <BusEta />
+                </Route>
+                <Route exact path="/train-eta">
+                  <TrainEta />
+                </Route>
+                <Route exact path="/weather">
+                  <Weather />
+                </Route>
+                <Route path="*">
+                  <NoMatch />
+                </Route>
+            </Switch>
+          </Grid>
+        </Grid>
       </div>
     </Router>
   );
