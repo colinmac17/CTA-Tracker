6abcd2cec725b7ce3285dfa7e585af0f32b661d3
Pedro Rebollar <prebolla15@gmail.com>
2020-10-26 15:09:48 -0500

    created outline of bus & train eta

--- src/Components/TrainEta.js
+++ src/Components/TrainEta.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import '../index.css';
+import Button from '@material-ui/core/Button';
+
+
+
 
 export default function TrainEta() {
-    return <h1>This is the train eta page</h1>;
+
+    const trainLine = ["Red", "Blue", "Green", "Yellow", "Brown", "Purple", "Orange"];
+    const items = [];
+
+    return (
+        <div>
+            <h2>Select a Train Line</h2>
+                {trainLine.map((value, index) => {
+                    return <button type='button' key={index} style={{backgroundColor: {value}}}> {value} </button>
+                })}
+
+            <hr/>
+            <button type='button' >View Trains</button>
+        </div>
+    
+    );
 }
