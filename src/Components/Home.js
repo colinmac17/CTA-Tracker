dd45f926d8aa050f4266da1ee292dbbfcba81a6e
puvvadaharsha <puvvadaharsha@gmail.com>
2020-11-08 17:26:56 -0600

    Removed old landing Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,49 +1,10 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-
-
-const TemperatureWidget = (props) => {
-    return(
-        <div>
-            <p>Current Temp: 33°F</p>
-        </div>
-    )
-}
-
-const AlertWidget = (props) => {
-    return(
-        <div>
-            <h3>Current CTA alerts:</h3>
-            <ol>
-                <li>Alert 1: Info about alert 1</li>
-                <li>Alert 2: Info about alert 2</li>
-                <li>Alert 3: Info about alert 3</li>
-            </ol>
-        </div>
-    )
-}
 
 const Home = (props) => {
     return(
-            <div>
-                <TemperatureWidget />
-
-                <Link to="/bus-eta">
-                    <button>
-                        <img src="" alt="Bus Button"/>
-                        <p>Bus Button</p>
-                    </button>
-                </Link>
-
-                <Link to="/train-eta">
-                    <button>
-                        <img src="" alt="Train Button"/>
-                        <p>Train Button</p>
-                    </button>
-                </Link>
-    
-                <AlertWidget />
-            </div>
+        <div>
+            
+        </div>
     )
 }
 
